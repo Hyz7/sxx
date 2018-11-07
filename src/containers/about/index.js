a971197904d828cec9fc4817856ddff8c95d21aa
769062519 <769062159.com>
2018-11-07 09:49:17 +0800

    update

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -4,7 +4,7 @@ export default class About extends Component{
     render() {
         return(
             <div>
-                about page!
+                this is about page!
             </div>
         )
     }
