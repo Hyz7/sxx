671e61c61f2f3c8d2fdd4cf7b951d844da411b48
769062519 <769062159.com>
2018-11-28 20:45:22 +0800

    update

--- src/components/teacher/index.js
+++ src/components/teacher/index.js
@@ -21,7 +21,19 @@ class Teacher extends Component{
                     </ul>
                     <div className="teacher-list">
                         <div className="teacher-title">专家大牛全程面授</div>
-                        <div className="list"></div>
+                        <div className="list">
+                            <div className="avatar"></div>
+                            <div className="right-box">
+                                <div className="name"> <i></i>张小松<span>XiaoSong Zhang</span></div>
+                                <div className="position">教务资深顾问</div>
+                                <div className="box">
+                                    <div className="text">长江学者特聘教授</div>
+                                    <div className="text">电子科技大学博士生导师</div>
+                                </div>
+                                <div className="goodAt">擅长领域：网络安全、大数据应用、区块链</div>
+                                <div className="goodAt">简介：国家重点研发计划网络空间安全专项首席科学家，2017国家十大网络安全优秀人才，电子科技大学网络空间安全研究中心主任，政府治理大数据应用技术国家工程实验室技术专家委员会副主任，国防科技工业网络安全创新中心专家委员会委员，教育部科技委国防学部委员，四川省学术技术带头人。长期致力于网络信息技术安全领域的基础研究和技术攻关，成果在应用中取得重要的社会和经济效益。</div>
+                            </div>
+                        </div>
                         <div className="list"></div>
                         <div className="list"></div>
                         <div className="list"></div>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import App from './app'
+// import App from './app'
+import store from "./store";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import MainRoutes from "./routes/home";
+import NotFound from "./components/notFound";
+import Provider from "react-redux/es/components/Provider";
+import './style/swiper/swiper.css'
+import './style/entry.less'
+
+const App=()=>{
+    return(
+        <Provider store={store}>
+            <BrowserRouter>
+                <Switch>
+                    <Route path='/' component={ MainRoutes }></Route>
+
+                    <Route component={ NotFound }/>
+                </Switch>
+            </BrowserRouter>
+        </Provider>
+    )
+}
 
 ReactDom.render(<App />,document.getElementById('root'))
