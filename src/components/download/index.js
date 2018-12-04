93d889c9cf341d058b65ae46853e6fb643c68d0b
769062519 <769062159.com>
2018-12-04 17:32:35 +0800

    update

--- src/components/download/index.js
+++ src/components/download/index.js
@@ -2,6 +2,14 @@ import React,{Component} from 'react'
 import {withRouter} from 'react-router-dom'
 import DownloadDetail from './downloadDetail.js'
 import sxxImg from '../../images/sxxbanner.png'
+import AAA from '../../images/download/AAA.png'
+import HMS from '../../images/download/HMS.png'
+import IBM from '../../images/download/IBM.png'
+import MATRIX from '../../images/download/MATRIX.png'
+import sharespost from '../../images/download/sharespost.png'
+import xgh from '../../images/download/xgh.png'
+import zq from '../../images/download/zq.png'
+
 class Download extends Component{
     constructor(){
         super()
@@ -58,7 +66,7 @@ class Download extends Component{
                         </div>
                         <div className="list-box">
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={AAA} alt=""/>
                                 <div className="content">
                                     <div className="title">AAA全球应用联盟链白皮书</div>
                                     <div className="description">
@@ -72,56 +80,76 @@ class Download extends Component{
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={sharespost} alt=""/>
                                 <div className="content">
-                                    <div className="title">AAA全球应用联盟链白皮书</div>
+                                    <div className="title">SharesPost 项目投资者概览</div>
                                     <div className="description">
-                                        基于区块链技术重构App的生产关系，将每个App用户变为股东。
-                                        通过App红包等方式巩固用户对AAA Coin的使用粘性，持续培养用户交易能力。
-                                        联盟全球所有中小规模App，在整合后的庞大用户体量上，共享技术，
-                                        共享用户价值红利。
+                                        2018年美国证监会对代币进行监管，多数会归为证券类，SharesPost会成为美国第一家合规的证券类代币的交易平台。
                                     </div>
                                     <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
                                     <div className="download-btn">立即下载</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={IBM} alt=""/>
                                 <div className="content">
-                                    <div className="title">AAA全球应用联盟链白皮书</div>
+                                    <div className="title">IBM物联网白皮书：设备民主</div>
                                     <div className="description">
-                                        基于区块链技术重构App的生产关系，将每个App用户变为股东。
-                                        通过App红包等方式巩固用户对AAA Coin的使用粘性，持续培养用户交易能力。
-                                        联盟全球所有中小规模App，在整合后的庞大用户体量上，共享技术，
-                                        共享用户价值红利。
+                                        IBM 作为一家全球性的电子企业，理解高科技行业正面临的问题，和为保持繁荣所需要的不
+                                        断变革。在整个电子行业，所有的企业正在将注意力从智能手机和平板领域，
+                                        转移到新一代的可连接设备，这不仅会变革电子行业，而且还会对其它行业产生深远的影响。
                                     </div>
                                     <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
                                     <div className="download-btn">立即下载</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={xgh} alt=""/>
                                 <div className="content">
-                                    <div className="title">AAA全球应用联盟链白皮书</div>
+                                    <div className="title">Weridfoxes小怪狐白皮书</div>
                                     <div className="description">
-                                        基于区块链技术重构App的生产关系，将每个App用户变为股东。
-                                        通过App红包等方式巩固用户对AAA Coin的使用粘性，持续培养用户交易能力。
-                                        联盟全球所有中小规模App，在整合后的庞大用户体量上，共享技术，
-                                        共享用户价值红利。
+                                        这是一款即兼顾了投资价值，也有浓重的宠物趣味性和互动性的游戏，是一款真正能让用户从心里喜欢，长久陪伴的宠物。
                                     </div>
                                     <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
                                     <div className="download-btn">立即下载</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={MATRIX} alt=""/>
                                 <div className="content">
-                                    <div className="title">AAA全球应用联盟链白皮书</div>
+                                    <div className="title">MATRIX 人工智能项目白皮书</div>
                                     <div className="description">
-                                        基于区块链技术重构App的生产关系，将每个App用户变为股东。
-                                        通过App红包等方式巩固用户对AAA Coin的使用粘性，持续培养用户交易能力。
-                                        联盟全球所有中小规模App，在整合后的庞大用户体量上，共享技术，
-                                        共享用户价值红利。
+                                        MATRIX 历史性地将人工智能技术和区块链技术全方位融合
+                                        ，涵盖了链上参数、共识算法、智能合约、应用服务等各个方面。MATRIX 团
+                                        队作为人工智能区块链的首创者和践行者，目标是通过 MATRIX 正式定
+                                        义下一代区块链技术，即区块链 3.0。
+                                    </div>
+                                    <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
+                                    <div className="download-btn">立即下载</div>
+                                </div>
+                            </div>
+                            <div className="list">
+                                <img src={zq} alt=""/>
+                                <div className="content">
+                                    <div className="title">兴业证券：平安加盟N3，中国正式登上区块链国际舞台</div>
+                                    <div className="description">
+                                        R3是目前世界上知名度最高的区块链联盟之一，已经有超过40家金融机构
+                                        加入该组织，其中包括摩根大通、高盛、花旗银行、美林证券、瑞士信贷、丹麦银行、德
+                                        意志银行等金融巨头。此次平安与R3的合作，是中国大型金融机构首次加入国际区块链联盟。
+                                    </div>
+                                    <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
+                                    <div className="download-btn">立即下载</div>
+                                </div>
+                            </div>
+                            <div className="list">
+                                <img src={HMS} alt=""/>
+                                <div className="content">
+                                    <div className="title">HMS健康互助社区白皮书</div>
+                                    <div className="description">
+                                        健康保障，是全人类的刚需问题。几乎所有家庭都担心巨额的危重疾病治疗费用和丧失劳动能力
+                                        的收入损失压垮整个家庭，目前解决途径主要有三种：商业保险、相互保险、互助社区。这些方
+                                        案都存在着诸多本质痛点，强烈压抑着用户的需求。
+                                        若出现一种可解决这些痛点的变革，刚需将得到释放，用户必然爆发式增长
                                     </div>
                                     <div className="bottom-text">标签：交易平台    数字钱包 <span>下载次数18</span></div>
                                     <div className="download-btn">立即下载</div>
@@ -136,7 +164,7 @@ class Download extends Component{
                         </div>
                         <div className="list-box">
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={sharespost} alt=""/>
                                 <div className="content">
                                     <div className="title">SharesPost 项目投资者概览</div>
                                     <div className="description">2018年美国证监会对代币进行监管，多数会归为证券类...</div>
@@ -144,34 +172,35 @@ class Download extends Component{
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={IBM} alt=""/>
                                 <div className="content">
-                                    <div className="title">SharesPost 项目投资者概览</div>
-                                    <div className="description">2018年美国证监会对代币进行监管，多数会归为证券类...</div>
+                                    <div className="title">IBM物联网白皮书：设备民主</div>
+                                    <div className="description">IBM 作为一家全球性的电子企业，理解高科技行业正面临的问题...</div>
                                     <div className="time">2018-11-8</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={xgh} alt=""/>
                                 <div className="content">
-                                    <div className="title">SharesPost 项目投资者概览</div>
-                                    <div className="description">2018年美国证监会对代币进行监管，多数会归为证券类...</div>
+                                    <div className="title">Weridfoxes小怪狐白皮书</div>
+                                    <div className="description">IBM 作为一家全球性的电子企业，理解高科技行业正面临的问题...</div>
                                     <div className="time">2018-11-8</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={MATRIX} alt=""/>
                                 <div className="content">
-                                    <div className="title">SharesPost 项目投资者概览</div>
-                                    <div className="description">2018年美国证监会对代币进行监管，多数会归为证券类...</div>
+                                    <div className="title">MATRIX 人工智能项目白皮书
+                                        第十七届西博会</div>
+                                    <div className="description">MATRIX 历史性地将人工智能技术和区块链技术全方位融合...</div>
                                     <div className="time">2018-11-8</div>
                                 </div>
                             </div>
                             <div className="list">
-                                <img src="" alt=""/>
+                                <img src={zq} alt=""/>
                                 <div className="content">
-                                    <div className="title">SharesPost 项目投资者概览</div>
-                                    <div className="description">2018年美国证监会对代币进行监管，多数会归为证券类...</div>
+                                    <div className="title">兴业证券：平安加盟N3...</div>
+                                    <div className="description">R3是目前世界上知名度最高的区...</div>
                                     <div className="time">2018-11-8</div>
                                 </div>
                             </div>
--- src/components/sixuexing/index.js
+++ src/components/sixuexing/index.js
@@ -1,7 +1,15 @@
 import React,{Component} from 'react'
 import {withRouter} from 'react-router-dom'
 import NewsDetail from './newsDetail.js'
+import first from '../../images/sxx/first.png'
+import second from '../../images/sxx/second.png'
+import third from '../../images/sxx/third.png'
+import xibo from '../../images/sxx/xibo.png'
+import cansai from '../../images/sxx/cansai.png'
+import huang from '../../images/sxx/huang.png'
+import erdengjiang from '../../images/sxx/erdengjiang.png'
 import sxxImg from '../../images/sxxbanner.png'
+
 class Sxx extends Component{
     componentDidMount(){
         console.log(this.props)
@@ -25,7 +33,7 @@ class Sxx extends Component{
                         </div>
                         <ul className='news-list'>
                             <li>
-                                <span></span>
+                                <img src={third} alt=""/>
                                 <div className="news-content">
                                     <div className="list-title">第三期 成都区块链企业沙龙成功举办</div>
                                     <div className="description">11月8日下午，第三期成都区块链企业沙龙在银泰in99梦想加举行。 本期沙龙由成都市大数据协会区块链专委会主办，由拉勾网承办，亚马逊云计算、
@@ -34,7 +42,7 @@ class Sxx extends Component{
                                 </div>
                             </li>
                             <li>
-                                <span></span>
+                                <img src={second}></img>
                                 <div className="news-content">
                                     <div className="list-title">“区块链的发展与合作”成都区块链企业沙龙第二期成功举办</div>
                                     <div className="description">10月19日下午，第二期成都区块链企业沙龙在天府软件园举行。
@@ -43,7 +51,7 @@ class Sxx extends Component{
                                 </div>
                             </li>
                             <li>
-                                <span></span>
+                                <img src={first} alt=""/>
                                 <div className="news-content">
                                     <div className="list-title">首期成都区块链企业沙龙线下聚会活动成功举办</div>
                                     <div className="description">9月28日下午，由成都市大数据协会区块链专委会组织的首期“成都区块链企业沙龙线下聚会活动”在成都软件园优聚+孵化器举办......</div>
@@ -51,7 +59,16 @@ class Sxx extends Component{
                                 </div>
                             </li>
                             <li>
-                                <span></span>
+                                <img src={xibo} alt=""/>
+                                <div className="news-content">
+                                    <div className="list-title">“链接新时代，培养新人才”成都思学行教育亮相
+                                        第十七届西博会</div>
+                                    <div className="description">第十七届中国西部国际博览会以“中国新时代西部新作为”为主题，于2018年9月20日至24日在四川成都举办。“中国西部数字经济展”作为.....</div>
+                                    <div className="time">2018-9-24</div>
+                                </div>
+                            </li>
+                            <li>
+                                <img src={xibo} alt=""/>
                                 <div className="news-content">
                                     <div className="list-title">“链接新时代，培养新人才”成都思学行教育亮相
                                         第十七届西博会</div>
@@ -91,6 +108,22 @@ class Sxx extends Component{
                                 <span></span>
                                 <div className="text">【人民日报】智能网呼唤“全链安防”</div>
                             </div>
+                            <div className="industry-list">
+                                <span></span>
+                                <div className="text">【知识库】行业资讯第一篇：易观：2018中国区块链应用专题分析</div>
+                            </div>
+                            <div className="industry-list">
+                                <span></span>
+                                <div className="text">【百度】区块链在列 ｜ 大数据未来七大发展方向</div>
+                            </div>
+                            <div className="industry-list">
+                                <span></span>
+                                <div className="text">【百度】人工智能，描绘万物互联的未来</div>
+                            </div>
+                            <div className="industry-list">
+                                <span></span>
+                                <div className="text">【百度】区块链+AI，恰似双剑合璧？</div>
+                            </div>
                         </div>
                     </div>
                     <div className="sxx-box sxx-student">
@@ -100,7 +133,7 @@ class Sxx extends Component{
                         </div>
                         <ul className='student-list'>
                             <li>
-                                <span></span>
+                                <div className="box"><img src={cansai} alt=""/></div>
                                 <div className="news-content">
                                     <div className="list-title">区块链课程学员参赛</div>
                                     <div className="description">桂勋老师带领学员参加由工信部指导的“首届全国区块链开发大赛”并荣获二等奖</div>
@@ -108,10 +141,26 @@ class Sxx extends Component{
                                 </div>
                             </li>
                             <li>
-                                <span></span>
+                                <div className="box"><img src={huang} alt=""/></div>
                                 <div className="news-content">
                                     <div className="list-title">“区块链体验课“学员采访</div>
                                     <div className="description">黄同学：试过，才知道自己有多厉害！！</div>
+                                    <div className="time">2018-9-10</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="box"><img src={erdengjiang} alt=""/></div>
+                                <div className="news-content">
+                                    <div className="list-title">区块链开发大赛二等奖</div>
+                                    <div className="description">学员的努力和汗水！</div>
+                                    <div className="time">2018-9-28</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="box"><img src={third} alt=""/></div>
+                                <div className="news-content">
+                                    <div className="list-title">“区块链体验课“顺利开展</div>
+                                    <div className="description">区块链第一课，在天府新谷顺利开展</div>
                                     <div className="time">2018-9-10</div>
                                 </div>
                             </li>
