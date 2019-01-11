91f09653c30359b1ec149fae4cd65381aff63f7b
hyz <hyz_1mail@sina.com>
2019-01-11 14:28:20 +0800

    update

--- src/components/sixuexing/store/actionCreators.js
+++ src/components/sixuexing/store/actionCreators.js
@@ -67,6 +67,7 @@ export const getMoreList=(page,size)=>{
 export const getNewsList=(value)=>{
     return (dispatch)=>{
         axios.get(API.GET_NEWS_LIST+"?name="+value).then(res=>{
+            console.log(res.data)
             dispatch(createNewsList(res.data))
         })
     }
