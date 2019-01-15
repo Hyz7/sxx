d4f19170ada2e3f3c287f1c90d024cbe35f7cd6a
hyz <hyz_1mail@sina.com>
2019-01-15 16:41:59 +0800

    修改https

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 let env = process.env.NODE_ENV
 
-const HOST = env === 'production'? 'https://52.83.225.97:30000' : 'http://192.168.0.102:31000'
+const HOST = env === 'production'? 'https://www.sixuexing.com:31000' : 'http://192.168.0.102:31000'
 // http://192.168.0.102:8080
 export const GET_MENU_LIST = HOST+'/getTitle'
 
